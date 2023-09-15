7bc6085c749d402508af611628b157e246a45e83
Ashmoore <theodore.daniel@epitech.eu>
2023-09-15 15:11:07 +0200

    Adding blank controllers: auth, chat, comment, publication and user

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PublicationModule } from './publication/publication.module';
+import { CommentModule } from './comment/comment.module';
+import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    PublicationModule,
+    CommentModule,
+    ChatModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
