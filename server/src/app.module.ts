6aac2c6ac7ebd05fa4044c3797134b579ff8275a
Ashmoore <theodore.daniel@epitech.eu>
2023-09-15 16:38:50 +0200

    refactor: id -> userId, and remove useless route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,14 +8,6 @@ import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { JwtModule } from '@nestjs/jwt';
 import { NotificationModule } from './notification/notification.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PublicationModule } from './publication/publication.module';
-import { CommentModule } from './comment/comment.module';
-import { ChatModule } from './chat/chat.module';
-import { AuthModule } from './auth/auth.module';
-import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -39,7 +31,5 @@ import { AdminModule } from './admin/admin.module';
     AdminModule,
     NotificationModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
