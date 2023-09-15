60a9cde2cfdc994a97cd6e4fda779079289ced94
Ashmoore <theodore.daniel@epitech.eu>
2023-09-15 16:08:01 +0200

    Adding admin controller unimplemented, and basic user entity

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from './role';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uid: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  hash: string;
+
+  @Column()
+  role: Role;
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,26 +1,27 @@
+import { User } from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return new User();
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(): Promise<User[]> {
+    return [new User()];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    return new User();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return new User();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<void> {
+    return;
   }
 }
