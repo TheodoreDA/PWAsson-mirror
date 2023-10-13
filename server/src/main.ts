88744db5a80121e427dced1e446ecc88f2d6abaa
Ashmoore <theodore.daniel@epitech.eu>
2023-10-14 01:39:54 +0200

    Using ValidationPipe as Global

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     methods: 'GET, PATCH, POST, DELETE',
   });
 
-  await app.listen(8080);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (e) => {
+        console.error(e);
+        throw new BadRequestException(e[0]);
+      },
+    }),
+  );
+
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
