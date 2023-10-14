dc1bb076e0cbfb72314e8c192378631b5eff2d01
Ashmoore <theodore.daniel@epitech.eu>
2023-10-14 03:02:39 +0200

    Implementing fake Publication CRUD, like for the users

--- server/src/publication/publication.service.ts
+++ server/src/publication/publication.service.ts
@@ -1,26 +1,106 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePublicationDto } from './dto/create-publication.dto';
 import { UpdatePublicationDto } from './dto/update-publication.dto';
+import { Publication } from './entities/publication.entity';
+import { publicationFactory } from 'src/factory/publication.factory';
 
 @Injectable()
 export class PublicationService {
-  create(createPublicationDto: CreatePublicationDto) {
-    return 'This action adds a new publication';
+  private publications: Publication[] = [];
+
+  constructor() {
+    this.publications.push(
+      publicationFactory
+        .setUid('1234567890')
+        .setTitle('Title')
+        .setDescription('Description')
+        .setPictureUid('picture1')
+        .setAuthorUid('author1')
+        .setLikes(42)
+        .build(),
+    );
+    this.publications.push(
+      publicationFactory
+        .setUid('0987654321')
+        .setTitle('Title2')
+        .setDescription('Description2')
+        .setPictureUid('picture2')
+        .setAuthorUid('author2')
+        .setLikes(1)
+        .build(),
+    );
+    this.publications.push(
+      publicationFactory
+        .setUid('6543210987')
+        .setTitle('Title3')
+        .setDescription('Description3')
+        .setPictureUid('picture3')
+        .setAuthorUid('author3')
+        .setLikes(6)
+        .build(),
+    );
+  }
+
+  create(createPublicationDto: CreatePublicationDto, authorUid: string) {
+    const publication = publicationFactory
+      .setUid('6543210987')
+      .setTitle(createPublicationDto.title)
+      .setDescription(createPublicationDto.description)
+      .setPictureUid('picture3')
+      .setAuthorUid(authorUid)
+      .build();
+
+    this.publications.push(publication);
+    return publication;
   }
 
   findAll() {
-    return `This action returns all publication`;
+    return this.publications;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} publication`;
+  findOne(publicationId: string) {
+    const publication = this.publications.find(
+      (publication) => publication.uid == publicationId,
+    );
+
+    if (publication == undefined)
+      throw new NotFoundException(
+        "Could not find publication with id '" + publicationId + "'",
+      );
+    return publication;
   }
 
-  update(id: number, updatePublicationDto: UpdatePublicationDto) {
-    return `This action updates a #${id} publication`;
+  // TODO: Check if requesting user is the author
+  update(publicationId: string, updatePublicationDto: UpdatePublicationDto) {
+    const publicationIdx = this.publications.findIndex(
+      (publication) => publication.uid == publicationId,
+    );
+
+    if (publicationIdx == -1) {
+      throw new NotFoundException(
+        "Could not find publication '" + publicationId + "'",
+      );
+    }
+    if (updatePublicationDto.title)
+      this.publications[publicationIdx].title = updatePublicationDto.title;
+    if (updatePublicationDto.description) {
+      this.publications[publicationIdx].description =
+        updatePublicationDto.description;
+    }
+    return this.publications[publicationIdx];
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} publication`;
+  // TODO: Check if requesting user is the author
+  remove(publicationId: string) {
+    const publicationIdx = this.publications.findIndex(
+      (publication) => publication.uid == publicationId,
+    );
+
+    if (publicationIdx == -1) {
+      throw new NotFoundException(
+        "Could not find publication '" + publicationId + "'",
+      );
+    }
+    this.publications.splice(publicationIdx, 1);
   }
 }
