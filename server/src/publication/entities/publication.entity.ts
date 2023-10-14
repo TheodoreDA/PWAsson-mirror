c431dc45ff250615889289ddce1ba407d3973fc2
Ashmoore <theodore.daniel@epitech.eu>
2023-10-14 03:00:13 +0200

    Populate Publication Entity

--- server/src/publication/entities/publication.entity.ts
+++ server/src/publication/entities/publication.entity.ts
@@ -1,1 +1,10 @@
-export class Publication {}
+export class Publication {
+  uid: string;
+  title: string;
+  description: string;
+  pictureUid: string;
+  authorUid: string;
+  likes: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
