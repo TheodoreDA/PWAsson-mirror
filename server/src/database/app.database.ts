5128d7adfa758c61e796117f9189787a5191c547
Ashmoore <theodore.daniel@epitech.eu>
2023-10-14 13:49:26 +0200

    Init AppWrite database

---
+++ server/src/database/app.database.ts
@@ -0,0 +1,19 @@
+import { Client, Databases, Storage } from 'node-appwrite';
+
+let client: Client = null;
+let db: Databases = null;
+let storage: Storage = null;
+
+function InitAppWriteClient(): void {
+  client = new Client();
+
+  client
+    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  db = new Databases(client);
+  storage = new Storage(client);
+}
+
+export { db, storage, InitAppWriteClient };
