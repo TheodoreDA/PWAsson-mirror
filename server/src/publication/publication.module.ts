6e4d833554d651a4d5be90e15f8ef568d46b9732
Ashmoore <theodore.daniel@epitech.eu>
2023-10-14 03:01:04 +0200

    Adding JwtModule so AccessTokenInterceptor is usable

--- server/src/publication/publication.module.ts
+++ server/src/publication/publication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicationService } from './publication.service';
 import { PublicationController } from './publication.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [PublicationController],
   providers: [PublicationService],
 })
