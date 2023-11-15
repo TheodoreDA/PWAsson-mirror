c0160fbf563c29fd127d4689fb87b58423c3219c
eoxiaguillaume <70137982+eoxiaguillaume@users.noreply.github.com>
2023-11-15 15:57:26 +0100

    adding request on page Post and link to page post from feed and link to feed from newpost after posting something

--- cra-pwa/src/pages/Post.js
+++ cra-pwa/src/pages/Post.js
@@ -2,45 +2,87 @@ import { IoMdArrowBack } from "react-icons/io";
 import { BiSolidCommentDetail } from "react-icons/bi";
 import { FaHeart } from "react-icons/fa";
 import './Post.css';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 function Comment(props) {
     return <li>
-        <div>{ props.comment.user } <p className="time">{ props.comment.time }</p></div>
+        <div>{props.comment.user} <p className="time">{props.comment.formattedDate}</p></div>
         <div>
-            <p>{ props.comment.content }</p>
-            <div className="social-sm"><FaHeart/> { props.comment.likesNbr }</div>
+            <p>{props.comment.content}</p>
+            <div className="social-sm"><FaHeart /> {props.comment.likes}</div>
         </div>
     </li>;
 }
 
 function Post() {
-    const comments = [
-        { id: 1, user: "Mr Poulpe", time: "il y a 5 mins", content: "Je suis pas vraiment d'accord avec toi en vrai. Ma mère elle est plus belle", likesNbr: 14 },
-        { id: 1, user: "Mr Propre", time: "il y a 55 mins", content: "Je suis une huître", likesNbr: 140 }
-    ];
-    const post = { id: 1, user: "Ashmoore", title: "Regardez ma chèvre, elle est trop belle", time: "il y a 1 heure", img: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQ_roUCfAO8PBWPLvOM-DrYsUboxcf3tgMobBtqKqviYGRIiwpy0AW5BNBfDOe4JbKcjMFT8zsgA5bQcFU", commentsNbr: comments.length, likesNbr: 1000 };
-    const user = "Mr Poulpe";
-    const commentsList = comments.map((comment, index) => <Comment key={ "comment-"+index } comment={comment}/>);
+    let { state } = useLocation()
+    const [commentArray, setCommentArray] = useState([]);
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        // second: 'numeric',
+        // timeZoneName: 'short'
+    };
+
+    console.log(state.post);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get(`http://localhost:8080/comment/${state.post.uid}`);
+            let tmpCommentArray = []
+            for (let i = 0; i < response.data.length; i++) {
+                let tmpComment = {}
+                Object.assign(tmpComment,
+                    {
+                        uid: response.data[i]?.uid,
+                        content: response.data[i]?.content,
+                        author: response.data[i]?.author,
+                        likes: response.data[i]?.likes,
+                        createdAt: response.data[i]?.createdAt,
+                    });
+                const responseAuthor = await axios.get(`http://localhost:8080/user/${tmpComment.author}`)
+                const user = responseAuthor.data?.username;
+                Object.assign(tmpComment, { user: user });
+                const date = new Date(tmpComment.createdAt);
+                const formattedDate = date.toLocaleString('en-US', options);
+                console.log(formattedDate);
+                Object.assign(tmpComment, { formattedDate: formattedDate })
+                tmpCommentArray.push(tmpComment)
+            }
+            setCommentArray(tmpCommentArray);
+        }
+        console.log(commentArray);
+        fetchData();
+    }, [])
+
+    const post = { id: state.post.uid, user: state.post.user, title: state.post.title, img: state.post.image, commentsNbr: commentArray.length, likesNbr: state.post.likes };
+
+    const commentsList = commentArray.map((comment, index) => <Comment key={"comment-" + index} comment={comment} />);
 
     return (
         <div className="Post">
             <div className="header">
-                <div><Link to="/feed" className="return-cursor"><IoMdArrowBack/>Retour</Link></div>
-                <h1>{ post.title }</h1>
-                <Link to="/profile" className="username cursor-pointer">{ user }</Link>
+                <div><Link to="/feed" className="return-cursor"><IoMdArrowBack />Retour</Link></div>
+                <h1>{post.title}</h1>
+                <Link to="/profile" className="username cursor-pointer">{localStorage.getItem("username")}</Link>
             </div>
             <div className="body">
                 <div className="post">
                     <div>
-                        <p>{ post.user }</p>
-                        <p className="time">{ post.time }</p>
-                        <div className="social"><BiSolidCommentDetail/> { post.commentsNbr }</div>
-                        <div className="social"><FaHeart/> { post.likesNbr }</div>
+                        <p>{post.user}</p>
+                        {/* <p className="time">{post.time}</p> */}
+                        <div className="social"><BiSolidCommentDetail /> {post.commentsNbr}</div>
+                        <div className="social"><FaHeart /> {post.likesNbr}</div>
                     </div>
-                    <img src={ post.img } alt={ post.title }/>
+                    <img src={`data:image/png;base64,${post.img}`} alt={post.title} />
                 </div>
-                <ul>{ commentsList }</ul>
+                <ul>{commentsList}</ul>
             </div>
         </div>
     );
