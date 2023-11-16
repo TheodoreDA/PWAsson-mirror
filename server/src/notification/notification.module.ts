f770cae7db4b51ea75f429cff0f6d1fadc03d4f8
lapprenti18 <pokspid@gmail.com>
2023-11-16 17:18:49 +0100

    add jwt module in the notification one

--- server/src/notification/notification.module.ts
+++ server/src/notification/notification.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
