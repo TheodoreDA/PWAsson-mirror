a8a63e024c34bdcec8d916657c1a00be4a022bd3
lapprenti18 <pokspid@gmail.com>
2023-11-17 13:07:31 +0100

    add to the notification module user module and export it

--- server/src/notification/notification.module.ts
+++ server/src/notification/notification.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, UserModule],
   controllers: [NotificationController],
   providers: [NotificationService],
+  exports: [NotificationService],
 })
 export class NotificationModule {}
