ae49e10afe76401a93914be0a5595a065bf9989b
lapprenti18 <pokspid@gmail.com>
2023-11-17 13:06:58 +0100

    complete de accept notification logic

--- server/src/notification/notification.service.ts
+++ server/src/notification/notification.service.ts
@@ -1,16 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AcceptNotificationDto } from './dto/accept-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
-import webpush from 'web-push';
+import { UserService } from 'src/user/user.service';
+import { DB_ID, db } from 'src/database/app.database';
+import { Publication } from 'src/publication/entities/publication.entity';
+import { User } from 'src/user/entities/user.entity';
+
+const webpush = require('web-push');
 
 @Injectable()
 export class NotificationService {
-  acceptNotification(acceptNotificationDto: AcceptNotificationDto) {
+  private readonly userService: UserService;
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  sendNotification(user: User, payload: object) {
     webpush.setVapidDetails(
-      'mailto:' + process.env.EMAIL,
+      process.env.EMAIL,
       process.env.PUBLIC_VAPID_KEY,
       process.env.PRIVATE_VAPID_KEY,
     );
+
+    webpush.sendNotification({
+      endpoint: user.endpoint,
+      expirationTime: user.expirationTime,
+      keys: {
+        p256dh: user.p256dh,
+        auth: user.auth,
+      }
+    }, JSON.stringify(payload));
+  }
+
+  async acceptNotification(acceptNotificationDto: AcceptNotificationDto, uid: string) {
+    const user = await this.userService.findOne(uid);
+
+    user.isNotifAllowed = true;
+    user.endpoint = acceptNotificationDto.endpoint;
+    user.expirationTime = acceptNotificationDto.expirationTime;
+    user.p256dh = acceptNotificationDto.keys.p256dh;
+    user.auth = acceptNotificationDto.keys.auth;
+    
+    try {
+      await db.updateDocument(DB_ID, 'USERS', uid, user);
+    } catch (e) {
+      throw new BadRequestException('UnknownException: ' + e.message);
+    }
+
+    const payload = {
+      notification: {
+          title: 'Well done you subscribed to notification',
+          body: 'this is a test notification',
+      },
+    };
+
+    this.sendNotification(user, payload);
+
     return 'This action adds a new Notification';
   }
 
